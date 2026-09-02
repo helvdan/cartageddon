ab78f6175d05b4b6676aefdc7a0330a810b2bcc5
Макеев Леонид <lmakeev@rbc.ru>
2026-09-02 15:30:01 +0300

    update license

--- extract.sh
+++
@@ -1,4 +0,0 @@
-. ./extract/set_env.sh
-#. ./open_ssh_tunnel.sh
-PYTHONPATH="/www/zoovostorg.com/etl" python3 extract/extract.py "$@"
-#. ./close_ssh_tunnel.sh
--- load.sh
+++
@@ -1,5 +0,0 @@
-#. ../../env/bin/activate
-. ./load/set_env.sh
-#. ./open_ssh_tunnel.sh
-PYTHONPATH="/www/zoovostorg.com/etl" python3 load/load.py
-#. ./close_ssh_tunnel.sh
